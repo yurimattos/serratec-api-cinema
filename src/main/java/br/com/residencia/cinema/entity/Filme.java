1af3f12a355f1f4e57211f86ef317ac2a1613058
Yuri <yurimattos96@gmail.com>
2022-10-11 22:16:54 -0300

    Adicionando tratamento para recursividade a nível de entidade

--- src/main/java/br/com/residencia/cinema/entity/Filme.java
+++ src/main/java/br/com/residencia/cinema/entity/Filme.java
@@ -10,7 +10,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property = "idFilme")
 @Entity
 @Table(name="filme")
 public class Filme {
--- src/main/java/br/com/residencia/cinema/entity/Genero.java
+++ src/main/java/br/com/residencia/cinema/entity/Genero.java
@@ -11,6 +11,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property = "idGenero")
 @Entity
 @Table(name="genero")
 public class Genero {
